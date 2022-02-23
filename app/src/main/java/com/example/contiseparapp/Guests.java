987bb0c9b951fef3d0af4b4beafeec4d71e9a4d5
serafinon <seranico2001@gmail.com>
2022-02-23 17:12:41 +0100

    sistemo la struttura del progetto

---
+++ app/src/main/java/com/example/contiseparapp/Guests.java
@@ -0,0 +1,35 @@
+package com.example.contiseparapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Guests {
+
+    private static ArrayList<Coppia> guests = new ArrayList<Coppia>();
+
+    public static void addEl(String nome, Double quota) {
+        Coppia coppia=new Coppia(nome,quota);
+        guests.add(coppia);
+    }
+
+    public static Coppia getEl(int index) {
+        return guests.get(index);
+    }
+
+    public static void delEl(int index) {
+        guests.remove(index);
+    }
+
+    public static int getSize(){
+        return guests.size();
+    }
+
+
+    public static Double calcSum(){
+        Double sum= 0.0;
+        for (Coppia c : guests) {
+            sum+=c.quota;
+        }
+        return sum;
+    }
+}
---
+++ app/src/main/java/com/example/contiseparapp/Results.java
@@ -0,0 +1,82 @@
+package com.example.contiseparapp;
+
+import java.util.ArrayList;
+
+public class Results{
+
+    private static ArrayList<Coppia> results;
+    private static Double monetine;
+
+
+    public static void addEl(String nome, Double quota) {
+        Coppia coppia=new Coppia(nome,quota);
+        results.add(coppia);
+    }
+
+    public static Coppia getEl(int index) {
+        return results.get(index);
+    }
+
+    public static Double getMonetine(){
+        return monetine;
+    }
+
+    public static void delEl(int index) {
+        results.remove(index);
+    }
+
+    public static int getSize() {
+        return results.size();
+    }
+
+    public Results(Double conto){
+
+        results = new ArrayList<Coppia>();
+
+        Double sum = Guests.calcSum();
+        Double differenza = conto - sum;
+
+        //PRIMA ITERAZIONE -> ripartisco la differenza in pezzi da 1€
+        Double totDecimal = Utility.round( differenza % Guests.getSize() , 2)  ;
+        Double riporto = Utility.round( (differenza - totDecimal)/Guests.getSize() , 2) ;
+
+        //SECONDA ITERAZIONE PER RIDURRE il TotDecimal (monetine)
+        //suddivido monetine in pezzi da 0.50€ tra i guests
+        if( (double)Guests.getSize()*0.5 <= totDecimal ){
+            riporto+=0.5;
+            totDecimal = Utility.round( totDecimal - ( 0.5 * Guests.getSize()), 2)  ;
+        }
+
+        //TERZA ITERAZIONE PER RIDURRE il TotDecimal (monetine)
+        //suddivido monetine in pezzi da 0.20€ tra i guests
+        while( (double)Guests.getSize()*0.2 <= totDecimal ){
+            riporto+=0.2;
+            totDecimal = Utility.round( totDecimal - ( 0.2 * Guests.getSize()) , 2 );
+        }
+
+        //QUARTA ITERAZIONE PER RIDURRE il TotDecimal (monetine)
+        //suddivido monetine in pezzi da 0.10€ tra i guests
+        while( (double)Guests.getSize()*0.1 <= totDecimal ){
+            riporto+=0.1;
+            totDecimal = Utility.round(totDecimal - ( 0.1 * Guests.getSize()) , 2 );
+        }
+
+        //Double riportoIntero = riporto - riportoDecimal;
+        //Double TotDecimal = riportoDecimal * guests.size();
+
+        System.out.println("conto = " + conto + "\nsum = " + sum + "\nriporto = " + riporto + "\ntotDecimal = " + totDecimal);
+
+        //aggiorno i risultati
+        for (int i=0;i<Guests.getSize();i++) {
+            Results.addEl( Guests.getEl(i).nome, Guests.getEl(i).quota + riporto);
+        }
+
+        //monetine contiene quella porzione di conto che non è suddivisibile ulteriormente tra le persone
+        monetine = totDecimal;
+
+    }
+
+    //public static void calcRes(Double conto) {}
+
+
+}
